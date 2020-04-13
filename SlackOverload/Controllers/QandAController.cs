df37485472fd46e6814161771a462685e30b94e7
Peter Guenther <peter@grandcircus.co>
2020-04-13 14:01:21 -0400

    first commit

---
+++ SlackOverload/Controllers/QandAController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SlackOverload.Models;
+
+namespace SlackOverload.Controllers
+{
+    public class QandAController : Controller
+    {
+        private DAL dal;
+
+        public QandAController(IConfiguration config)
+        {
+            dal = new DAL(config.GetConnectionString("default"));
+        }
+
+        public IActionResult Index()
+        {
+            //get the most recent questions
+            IEnumerable<Question> results = dal.GetQuestionsMostRecent();
+
+            ViewData["Questions"] = results;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new Question());
+        }
+
+        [HttpPost]
+        public IActionResult Add(Question q)
+        {
+            int result = dal.CreateQuestion(q);
+            
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Detail(int id) {
+            //first get the question detail
+            Question question = dal.GetQuestionById(id);
+
+            //then get the relevant answers
+            IEnumerable<Answer> answers = dal.GetAnswersByQuestionId(id);
+
+            ViewData["Answers"] = answers;
+
+            return View(question);
+        }
+    }
+}
---
+++ SlackOverload/Models/DAL.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlackOverload.Models
+{
+    public class DAL
+    {
+        private SqlConnection conn;
+
+        public DAL(string connectionString)
+        {
+            conn = new SqlConnection(connectionString);
+        }
+
+        public int CreateQuestion(Question q)
+        {
+            q.Posted = DateTime.Now;
+            q.Status = 1; //always create status=1
+
+            string addQuery = "INSERT INTO Questions (Username, Title, Detail, Posted, Category, Tags, Status) ";
+            addQuery += "VALUES (@Username, @Title, @Detail, @Posted, @Category, @Tags, @Status)"; 
+
+            return conn.Execute(addQuery, q);
+        }
+
+        public IEnumerable<Answer> GetAnswersByQuestionId(int id)
+        {
+            string queryString = "SELECT * FROM Answers WHERE QuestionId = @id";
+            return conn.Query<Answer>(queryString, new { id = id });
+        }
+
+        public Question GetQuestionById(int id)
+        {
+            string queryString = "SELECT * FROM Questions WHERE Id = @id";
+            return conn.QueryFirstOrDefault<Question>(queryString, new { id = id });
+        }
+
+        public IEnumerable<Question> GetQuestionsMostRecent()
+        {
+            string queryString = "SELECT TOP 20 * FROM Questions ORDER BY Posted DESC";
+            return conn.Query<Question>(queryString);
+        }
+    }
+}
